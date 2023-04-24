00f1d9f7ad74a7cd5d978f4542629631b4ee2c85
Illia Kornyk <forest.bukson@gmail.com>
2023-04-24 13:16:06 +0300

    feature: create setTime method for counter

--- js/Counter.js
+++ js/Counter.js
@@ -1,4 +1,6 @@
 export class Counter {
+  static instance;
+
   #amountOfTime;
   #startTime = new Date();
   #endTime;
@@ -8,15 +10,25 @@ export class Counter {
   #resetCallback;
 
   constructor(amountOfTime = 25, updateCallback, resetCallback) {
+    if (Counter.instance) {
+      return Counter.instance;
+    }
+
     this.amountOfTime = amountOfTime;
     this.endTime = new Date(
       this.startTime.getTime() + this.amountOfTime * 60 * 1000
     );
     this.updateCallback = updateCallback;
     this.resetCallback = resetCallback;
+
+    Counter.instance = this;
   }
 
   #counterDown() {
+    if (!this.startTime || !this.endTime) {
+      this.#setTime(this.amountOfTime);
+    }
+
     this.intervalID = setInterval(() => {
       const secondsLeft = this.secondsLeft;
       const minutes = Math.floor(secondsLeft / 60);
@@ -51,6 +63,13 @@ export class Counter {
     const pauseTime = currentTime - this.stopTime;
     this.endTime = new Date(this.endTime.getTime() + pauseTime);
     this.#counterDown();
+  }
+
+  #setTime(amountOfTime = 25) {
+    this.#startTime = new Date();
+    this.#endTime = new Date(
+      this.#startTime.getTime() + amountOfTime * 60 * 1000
+    );
   }
 
   get startTime() {
